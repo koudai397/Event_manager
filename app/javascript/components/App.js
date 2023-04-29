7a6423457682d63088fa929e347b874811182294
koudai397 <koudai.jaden@gmail.com>
2023-04-30 07:42:53 +0900

    add: Routes,Routeコンポーネントを追加

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Editor from "./Editor";
 
-const App = () => <Editor />;
+const App = () => (
+  <Routes>
+    <Route path="events/*" element={<Editor />} />
+  </Routes>
+);
+// Routesコンポーネントを使うことによってeventsから始まるURLを表示するたびにレンダリングされるようになる
 
 export default App;
