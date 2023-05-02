cf5a954ec209886c106113670bbb5203ba5160bd
koudai397 <koudai.jaden@gmail.com>
2023-05-03 07:18:57 +0900

    add: コメントを修正しました

--- app/javascript/components/EventForm.js
+++ app/javascript/components/EventForm.js
@@ -1,5 +1,5 @@
-import React from "react";
 import React, { useState } from "react";
+import { isEmptyObject, validateEvent } from "../helpers/helpers";
 
 const EventForm = () => {
   const [event, setEvent] = useState({
@@ -14,42 +14,19 @@ const EventForm = () => {
   const [formErrors, setFormErrors] = useState({});
 
   const handleInputChange = (e) => {
+    //   この関数はフォームの入力値が変更されたときに呼び出される関数。
     const { target } = e;
-    //このeには、様々なイベントに関する情報が入っており、今回は入力された名前と値を抽出し、それをsetEvent関数をつかって、状態を管理している。
     const { name } = target;
-    //eにはイベントが発生した要素に関する情報が入っており、targetにはイベントが発生した要素自体を参照している。ここではtargetプロパティにあるnameだけを取り出している。
     const value = target.type === "checkbox" ? target.checked : target.value;
-    //フォーム欄の種類がcheckboxであれば、targetの中にcheckedの値を代入して、checked以外の場合は通常のフォーム欄の値としてvalueを代入している
-
+    //e(イベントオブジェクト)には様々なイベントに関する情報が入っており、今回はtargetからイベントが発生した要素自体を参照している。次にtargetプロパティからnameプロパティを取り出している。
+    //そして、最後にフォーム欄の種類がcheckboxであれば、targetの中にcheckedの値を代入して、checkbox以外の場合は通常のフォーム欄の値としてvalueを代入している
     setEvent({ ...event, [name]: value });
-    //eventオブジェクトのコピーを作って、nameとvalueを更新している。
+    //そして、与えられた情報を元にsetEvent関数を使って、eventオブジェクトのコピーを作って、nameとvalueを更新している。
   };
-
-  const validateEvent = () => {
-    const errors = {};
-
-    if (event.event_type === "") {
-      errors.event_type = "You must enter an event type";
-    }
-    if (event.event_date === "") {
-      errors.event_date = "You must enter a valid date";
-    }
-    if (event.title === "") {
-      errors.title = "You must enter a title";
-    }
-    if (event.speaker === "") {
-      errors.speaker = "You must enter at least one speaker";
-    }
-    if (event.host === "") {
-      errors.host = "You must enter at least one host";
-    }
-    return errors;
-  };
-
-  const isEmptyObject = (obj) => Object.keys(obj).length === 0;
 
   const renderErrors = () => {
     if (isEmptyObject(formErrors)) {
+      // formErrorsが空でないか確認している
       return null;
     }
 
@@ -58,6 +35,8 @@ const EventForm = () => {
         <h3>The following errors prohibited the event from being saved:</h3>
         <ul>
           {Object.values(formErrors).map((formError) => (
+            //   Object.values()で配列を返している。
+            //   mapメソッドをつかい、formErrors配列から一つづつ取り出して、liタグを各要素に付与して表示さしている
             <li key={formError}>{formError}</li>
           ))}
         </ul>
@@ -66,14 +45,18 @@ const EventForm = () => {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
     // フォームの入力を処理するための関数 handleSubmit を定義しています。
-    // preventDefault()を呼び出さないと入力したフォームが送信されたときにブラウザがデフォルトのイベント動作を実行し、ページを再読み込みします。そうすると、ユーザーの入力した情報が消えてしまい、初期状態に戻ってしまうのでpreventDefaultを呼び出しています
+    // preventDefault()は、イベントが持つデフォルトの動作をキャンセルするメソッド
+    // これをしないと、入力したフォームが送信されたときにブラウザがデフォルトのイベント動作を実行し、ページを再読み込みをする。そうすると、ユーザーの入力した情報が消えてしまい、初期状態に戻ってしまうのでpreventDefaultを呼び出している。
+    e.preventDefault();
     const errors = validateEvent(event);
+    //   validateEventを呼び出して現在のフォームの状態であるeventをerrorsに代入している
     if (!isEmptyObject(errors)) {
       setFormErrors(errors);
+      // ここでisEmptyObject関数を使用して中身が空でない場合, つまりフォームにエラーがある場合は、setFormErrors関数でエラーを表示させている。
     } else {
       console.log(event);
+      // 中身が空場合(エラーががない場合)は、コンソールにイベントを表示している
     }
   };
 
@@ -155,7 +138,7 @@ const EventForm = () => {
         <div className="form-actions">
           <button type="submit">Save</button>
         </div>
-        {/* type="submit"が指定されたボタンなので、↑form 要素の onSubmit ハンドラが呼び出されます */}
+        {/* type="submit"が指定されているボタンなので、↑form 要素の onSubmit ハンドラが呼び出されます */}
       </form>
     </section>
   );
